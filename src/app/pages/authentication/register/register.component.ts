25e5d268d417c0127033c848b7ec8469903578b6
Castedo_Daniel <117131469+pablodaniel28@users.noreply.github.com>
2024-06-08 01:08:09 -0400

    registrar

--- src/app/pages/authentication/register/register.component.ts
+++ src/app/pages/authentication/register/register.component.ts
@@ -1,26 +1,71 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { UsersService } from 'src/app/users.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
 })
 export class AppSideRegisterComponent {
-  constructor(private router: Router) {}
 
-  form = new FormGroup({
-    uname: new FormControl('', [Validators.required, Validators.minLength(6)]),
-    email: new FormControl('', [Validators.required]),
-    password: new FormControl('', [Validators.required]),
-  });
+  formData: any = {
+    name: '',
+    email: '',
+    password: '',
+    role: '',
+  };
+  errorMessage: string = '';
+  constructor(
+    private readonly userService: UsersService,
+    private readonly router: Router
+  ) {}
 
-  get f() {
-    return this.form.controls;
+  async handleSubmit() {
+    // Check if all fields are not empty
+    if (!this.formData.name || !this.formData.email || !this.formData.password) {
+      this.showError('Inserte todos los datos');
+      return;
+    }
+
+    // Confirm registration with user
+    const confirmRegistration = confirm('¿Confirmar registro?');
+    if (!confirmRegistration) {
+      return;
+    }
+
+    // Asignar automáticamente el rol si no se ha seleccionado ningún rol
+    if (!this.formData.role) {
+      this.formData.role = 'USER';
+    }
+
+    try {
+      const token = localStorage.getItem('token');
+      if (!token) {
+        throw new Error('No se encontró ningún token');
+      }
+
+      const response = await this.userService.register(this.formData, token);
+      if (response.statusCode === 200) {
+        this.router.navigate(['/dashboard']);
+      } else {
+        this.showError(response.message);
+      }
+    } catch (error: any) {
+      this.showError(error.message);
+    }
   }
 
-  submit() {
-    // console.log(this.form.value);
-    this.router.navigate(['/dashboard']);
+
+  showError(message: string) {
+    this.errorMessage = message;
+    setTimeout(() => {
+      this.errorMessage = ''; // Clear the error message after the specified duration
+    }, 3000);
   }
+
+  // submit() {
+  //   // console.log(this.form.value);
+  //   this.router.navigate(['/dashboard']);
+  // }
 }
