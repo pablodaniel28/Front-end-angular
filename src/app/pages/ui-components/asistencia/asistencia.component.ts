97ae8f2b1478df7eab057eef8e5c1d08b54de6ba
Castedo_Daniel <117131469+pablodaniel28@users.noreply.github.com>
2024-06-14 22:17:38 -0400

    asistencia mostras

--- src/app/pages/ui-components/asistencia/asistencia.component.ts
+++ src/app/pages/ui-components/asistencia/asistencia.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AsistenciasService } from 'src/app/asistencias.service';
+import { Asistencia } from 'src/app/models/asistencias';
 
 @Component({
   selector: 'app-asistencia',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './asistencia.component.scss'
 })
 export class AsistenciaComponent {
+  asistencia: Asistencia[] = [];
 
+  constructor(private asistenciaService: AsistenciasService) { }
+
+  ngOnInit(): void {
+    this.loadAsistencia();
+  }
+
+  async loadAsistencia() {
+    try {
+      const token = localStorage.getItem('token') || ''; // Obtén el token desde el localStorage
+      this.asistencia = await this.asistenciaService.getAllAsistencia(token);
+      console.log('Carreras cargadas:', this.asistencia); // Muestra las carreras cargadas en la consola
+    } catch (error) {
+      console.error('Error cargando carreras:', error); // Maneja los errores al cargar las carreras
+    }
+  }
 }
